e3bd4800bf0eb1320e23df37d1d45d16ebc283ae
Gabriel Coutinho <gcoutinhorj@gmail.com>
2025-01-15 15:37:33 -0300

    Update autogenerate_readme.sh

--- autogenerate_readme.sh
+++ autogenerate_readme.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-find . -not -path "./.*" -not -name "autogenerate_readme.sh" -not -name "README.md" |
+find . -not -path "./.*" -not -name "autogenerate_readme.sh" -not -name "README.md" -not -name "." |
   awk -F "/" '{ spaces = ""; for (i = 2; i < NF; i++) { spaces = sprintf("%s\t", spaces); } split($NF, file_name, "."); printf("%s - [%s](%s)\n", spaces, file_name[1], $0); }' >README.md
